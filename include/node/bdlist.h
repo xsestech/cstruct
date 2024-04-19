f3ca62b194b97d052df1eed2fb5e17bbd159c9b9
xsestech <xsestech@gmail.com>
2024-04-19 23:18:02 +0300

    Finished linked list

--- include/node/bdlist.h
+++ include/node/bdlist.h
@@ -5,9 +5,56 @@
 #ifndef BIDIRECTIONAL_LIST_H
 #define BIDIRECTIONAL_LIST_H
 
-typedef struct bdlist_node bdlist_node;
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
 
-void bdlist_init()
+typedef struct bdlist_node bdlist_node_t;
+typedef struct bdlist_node* bdlist_handle_t;
 
+/**
+ * Init list
+ * @return pointer to first node
+ */
+bdlist_handle_t bdlist_init();
+
+/**
+ * Destroys the list
+ * @param bdlist root node of the list
+ */
+void bdlist_destroy(bdlist_handle_t bdlist);
+
+/**
+ * Pushes value to the back of the linked list
+ * @param bdlist root node
+ * @param value value to push
+ */
+void bdlist_push_back(bdlist_handle_t bdlist, double value);
+/**
+ * Pops value from back of the list
+ * @param bdlist root node of the list
+ * @return poped value(NAN if list is empty)
+ */
+double bdlist_pop_back(bdlist_handle_t bdlist);
+
+/**
+ * Pushes value to front of list
+ * @param bdlist_ptr pointer to root node of the linked list
+ * @param value value to push to front of list
+ */
+void bdlist_push_front(bdlist_handle_t* bdlist_ptr, double value);
+/**
+ * Pops value from front of the list
+ * @param bdlist_ptr pointer to root node of the linked list
+ * @return poped value(NAN if list is empty)
+ */
+double bdlist_pop_front(bdlist_handle_t* bdlist_ptr);
+/**
+ * Checks if list is empty
+ * @param bdlist root node of the list
+ * @return true if empty
+ */
+bool bdlist_is_empty(bdlist_handle_t bdlist);
 
 #endif //BIDIRECTIONAL_LIST_H
